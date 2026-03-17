a32d67495cfc38ba1e93f4caea95bedd10db1f21
Roman <yummison44@gmail.com>
2026-03-17 13:37:30 +0100

    temp

---
+++ src/main/java/easv/easv_tickets_bar/gui/LoginController.java
@@ -0,0 +1,15 @@
+package easv.easv_tickets_bar.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML private TextField emailField;
+    @FXML private TextField passwordField;
+
+    @FXML
+    private void loginClick(){
+        return;
+    }
+}
